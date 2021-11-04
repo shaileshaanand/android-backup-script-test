ec9566985102693f78624a00bf5459887cfecba3
Shailesh Aanand <anaandshailu@gmail.com>
2021-11-04 18:38:50 +0530

    Add apk and data backup

--- abs.py
+++ abs.py
@@ -1,5 +1,4 @@
 import subprocess
-import shlex
 from pathlib import Path
 import sys
 
@@ -8,31 +7,43 @@ APP_ID = "com.snowcorp.stickerly.android"
 
 def prepare(backup_folder: Path):
     backup_folder.mkdir(exist_ok=True)
-    (backup_folder/"apps").mkdir(exist_ok=True)
+    (backup_folder / "apps").mkdir(exist_ok=True)
 
 
 def apk_backup(app_id: str, backup_folder: Path):
-    ##
-    print(app_id, backup_folder)
-    ##
-    ((backup_folder/"apps")/app_id).mkdir(exist_ok=True)
-    base_apk_path: str = subprocess.run(
-        ["pm", "path", app_id],
+    ((backup_folder / "apps") / app_id / "apks").mkdir(exist_ok=True, parents=True)
+    base_apk_path: Path = Path(subprocess.run(
+        ["su", "-c", f"pm path {app_id}"],
         capture_output=True
-    ).stdout.decode("utf-8").split(":")[1]
-    print("base_apk_path", base_apk_path)
-    return
+    ).stdout.decode("utf-8").split(":")[1])
+    apk_folder = base_apk_path.parent
+    apks = list(
+        map(
+            Path,
+            filter(
+                lambda item: item.endswith(".apk"),
+                subprocess.run(
+                    ["su", "-c", f"ls {apk_folder}"],
+                    capture_output=True
+                ).stdout.decode("utf-8").splitlines()
+            )
+        )
+    )
+    for apk in apks:
+        subprocess.run(
+            ["su", "-c", f"cp {apk_folder/apk} {str(backup_folder/'apps'/app_id/'apks')}"]
+        )
+
+
+def data_backup(app_id: str, backup_folder: Path):
+    ((backup_folder / "apps") / app_id / "data").mkdir(exist_ok=True, parents=True)
     subprocess.run(
-        [
-            "tar",
-            "-cvf",
-            f"{app_id}.appdata.tar",
-            f"/data/app/"
-        ]
+        ["su", "-c", f"tar -cvf {str(backup_folder/'apps'/app_id/'data'/(app_id+'.data.tar'))} /data/data/{app_id}"]
     )
 
 
 if __name__ == "__main__":
-    backup_path = "test_bak"
+    backup_path = Path(sys.argv[1])
     prepare(backup_path)
-    apk_backup("com.whatsapp", backup_path)
+    apk_backup("com.snowcorp.stickerly.android", backup_path)
+    data_backup("com.snowcorp.stickerly.android", backup_path)
